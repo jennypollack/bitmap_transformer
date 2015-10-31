40d8dac8e2d4553fea04a26b7d0e9ed7b74e4e5d
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-10-31 14:53:43 -0700

    uncomment listener

--- index.js
+++ index.js
@@ -3,5 +3,5 @@ var transform = require(__dirname + '/lib/transform');
 var something = new transform();
 console.log(something);
 
-// something.listener();
+something.listener();
 something.readFile('bitmap1.bmp');
