1c2d23ea193e9723d3b8e56c234650ce071b3040
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-10-31 11:46:58 -0700

    async load and convert working

--- bitmapinvert.js
+++ bitmapinvert.js
@@ -1,27 +1,57 @@
 var fs = require('fs');
-var bitmap = fs.readFileSync('bitmap1.bmp');
-var bitmapObject = {};
+var EE = require('events').EventEmitter;
 
-bitmapObject.headerField = bitmap.toString('utf-8',0 , 2);
-bitmapObject.size = bitmap.readUInt32LE(2);
-bitmapObject.pixelDataStart = bitmap.readUInt32LE(10);
-bitmapObject.dibHeaderSize = bitmap.readUInt32LE(14);
-bitmapObject.width = bitmap.readUInt32LE(18);
-bitmapObject.height = bitmap.readUInt32LE(22);
-bitmapObject.bitDepth = bitmap.readUInt16LE(28);
-bitmapObject.compressionMethod = bitmap.readUInt32LE(30);
-bitmapObject.numberColors = bitmap.readUInt32LE(46);
-bitmapObject.pixelData = bitmap.slice(1078, bitmap.length);
-bitmapObject.colorTable = bitmap.slice(54, 1078);
+var fileEvents = new EE();
+
+// THIS DOESN'T WORK BECAUSE bitmap IS UNDEFINED
+// WHILE FILE IS BEING READ
+// function readFile(file) {
+//   var bitmap = fs.readFile(file);
+//   console.log(bitmap);
+//   console.log("reading: " + file)
+//   fileEvents.emit('donebitmap', bitmap);
+// }
+
+function readFile(file) {
+  fs.readFile(file, function(err, data) {
+    if (err) return console.log(err);
+    fileEvents.emit('donebitmap', data);
+  });
+};
+
+
+function buildObject(buffer) {
+  // console.log(buffer);
+  var bitmapObject = {};
+
+  bitmapObject.headerField    = buffer.toString('utf-8',0 , 2);
+  bitmapObject.size           = buffer.readUInt32LE(2);
+  bitmapObject.pixelDataStart = buffer.readUInt32LE(10);
+  bitmapObject.dibHeaderSize  = buffer.readUInt32LE(14);
+  bitmapObject.width          = buffer.readUInt32LE(18);
+  bitmapObject.height         = buffer.readUInt32LE(22);
+  bitmapObject.bitDepth       = buffer.readUInt16LE(28);
+  bitmapObject.compressMeth   = buffer.readUInt32LE(30);
+  bitmapObject.numberColors   = buffer.readUInt32LE(46);
+  bitmapObject.pixelData      = buffer.slice(1078, buffer.length);
+  bitmapObject.colorTable     = buffer.slice(54, 1078);
+  // event emitter to do transform
+  return bitmapObject
+};
+
+fileEvents.on('donebitmap', function(data) {
+  console.log("creating object");
+  console.log(buildObject(data));
+});
+
+readFile('bitmap1.bmp');
+
+// Array.prototype.forEach.call(bitmapObject.colorTable, function(byte, index) {
+//   bitmapObject.colorTable.writeUInt8(255 - byte, index);
+// });
 
 // console.log(bitmapObject);
 
-Array.prototype.forEach.call(bitmapObject.colorTable, function(byte, index) {
-  bitmapObject.colorTable.writeUInt8(255 - byte, index);
-});
+// bitmap.writeUIntLE(bitmapObject.colorTable, 54);
 
-console.log(bitmapObject);
-
-bitmap.writeUIntLE(bitmapObject.colorTable, 54);
-
-fs.writeFileSync('transformedbitmap.bmp', bitmap);
+// fs.writeFileSync('transformedbitmap.bmp', bitmap);
