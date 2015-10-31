50936ec400b1270d19e6d1dde24cbe7b2653b1b2
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-10-31 14:42:28 -0700

    working module

--- index.js
+++ index.js
@@ -1,13 +1,7 @@
 var transform = require(__dirname + '/lib/transform');
 
-<<<<<<< HEAD
 var something = new transform();
 console.log(something);
-=======
 
-var something = new transform(); 
-console.log(something); 
->>>>>>> 429b55420a674beee96a66c555dc4ea5c24874cb
-
-something.listener();
+// something.listener();
 something.readFile('bitmap1.bmp');
