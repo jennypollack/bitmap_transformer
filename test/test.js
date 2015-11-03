d6606374adaeb4b69855c60e65d89ea915a117dd
Matthew Ringel <mattew.r.ringel@gmail.com>
2015-11-02 16:49:41 -0800

    async tests of readfile working

--- test/test.js
+++ test/test.js
@@ -2,18 +2,22 @@ var expect = require('chai').expect;
 var Transform = require(__dirname + '/../lib/transform');
 
 var transformer = new Transform();
-console.log(transformer);
+// console.log(transformer);
+myBuff = null;
 
-describe('readfile function', function(){
-  before(function(done) {
-    transformer.readFile(__dirname + '/../bitmap1.bmp');
+before(function(done) {
+  transformer.readFile(__dirname + '/../bitmap1.bmp', function(err, data) {
+    myBuff = data;
     done();
   });
+});
+
+describe('readfile function', function(){
+
   it('should create a buffer', function() {
-    console.log(transformer.bmpBuffer);
-    expect(transformer.bmpBuffer instanceof Buffer).to.be.true;
+    expect(myBuff instanceof Buffer).to.be.true;
   });
-  it('should be non-zero in size', function() {
-    expect(transformer.bmpBuffer).not.to.be.null;
+  it('should not be null', function() {
+    expect(myBuff).not.to.be.null;
   });
 });
