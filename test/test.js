d87743f493efc0f40a1b9639702204fa1250c4b1
Jenny Pollack <JennyPollack@Jennys-MacBook-Pro.local>
2015-11-02 16:16:59 -0800

    modified tests

--- test/test.js
+++ test/test.js
@@ -1,4 +1,5 @@
 var expect = require('chai').expect;
+var fs = require('fs');
 var Transform = require(__dirname + '/../lib/transform');
 
 var transformer = new Transform();
@@ -12,3 +13,46 @@ describe('readfile function', function(){
     done();
   });
 });
+
+//before(fs.stats()); 
+describe('checks for the file we create', function(){
+  it('checks to see if a file exists - it should fail', function(){
+
+    //this works - but the existsSync is deprecated... 
+    // expect(fs.existsSync('transformedbitmap.bmp')).to.eql(true);
+    // expect(fs.access)
+    console.log(fs.accessSync('transformedbitmap.bmp'));
+    expect(fs.accessSync('transformedbitmap.bmp').to.eql(true));
+   
+    // fs.readdir('.', function(err, files){
+    // //   //console.log(files); // list of all files in current directory
+    // //   //files.forEach(function(file){ 
+    //     var test = files[files.length-1];
+    //     console.log(typeof test);
+    //     console.log(test === 'test.bmp'); 
+    //     expect(test).to.eql('test.bmp');
+    // //   expect(test === 'test.bmp'); 
+    // //   //});
+    //  });
+    
+    //   , fs.F_OK, function (err))){
+    //   console.log('ugh'); 
+    
+    // });
+  });
+});
+
+
+
+// var fs = require('fs'),
+//   path = '/path/to/my/file',
+//   stats;
+
+// try {
+//   stats = fs.statSync(path);
+//   console.log("File exists.");
+// }
+// catch (e) {
+//   console.log("File does not exist.");
+// }
+
